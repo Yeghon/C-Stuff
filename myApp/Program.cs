f2a19fe9fa364bc8a92334d4308838251861fcab
Yeghon <haronyegon90@gmail.com>
2021-09-10 14:18:15 +0300

    🚀 Initial commit

---
+++ myApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace myApp
+{
+    class Program
+    {
+        /**
+        * Connecting to SQL database
+        * string
+        *    conString =
+        *        @"Data Source=localhost; Initial Catalog=expiry_registration;User ID=sa;Password=20304wW.";
+        * SqlConnection conn = new SqlConnection(conString);
+        */
+        static void Main(string[] args)
+        {
+            Console
+                .WriteLine("<========== Getting a database connection ============>");
+        }
+
+        /**
+        * Connecting to mysql
+        **/
+    }
+}
